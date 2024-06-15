f8d1071e78bfa46abc418947d3a0d0c348be493c
Jagjeet0518 <2201020285@cgu-odisha.ac.in>
2024-06-15 19:36:19 +0530

    mob section

--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -1,92 +1,116 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { BsFillPeopleFill } from "react-icons/bs";
-import Card from './Card';
+import Card from "./Card";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { FaPen } from "react-icons/fa";
 
 function Section() {
-    const menu=["All Posts","Article","Event","Education","Job"];
-   const posts = [
-  {
-    title: 'Article',
-    content: 'What if famous brands had regular fonts? Meet RegulaRands!',
-    truncatedContent: "I've worked in UX for the better part of a decade. From now on, I plan to rel...",
-    author: 'Sarthak Kamra',
-    views: '1.4k',
-    imageUrl: 'img1.png',
-    avatarUrl: 'p1.png'
-  },
-  {
-    title: '',
-    content: 'Tax Benefits for Investment under National Pension Scheme launched for Government',
-    truncatedContent: "I've worked in UX for the better part of a decade. From now on, I plan to rel...",
-    author: 'Sushil Misil',
-    views: '1.5k',
-    imageUrl: 'img2.png',
-    avatarUrl: 'p2.png'
-  },
-  {
-    title: 'Twitter',
-    content: 'Finance & Investment Elite: Social MNer-@LinkedEd',
-    truncatedContent: '#FinTwits #Fin #Trading #Stocks #Bonds #Investment #BusinessAnalytics #DataScience',
-    author: 'Wade Jones',
-    views: '1.7k',
-    imageUrl: 'img3.png',
-    avatarUrl: 'p3.png'
-  },
-  {
-    title: '',
-    content: 'Software Developer',
-    truncatedContent: '@MicrosoftSupport/Helpdesk • 4 hrs',
-    author: 'Joseph Gray',
-    views: '5.5k',
-    imageUrl: '',
-    avatarUrl: 'p4.png'
-  }
-];
-    return (
-        <>
-        <div className="w-[80%] min-h-[100vh]  mx-[200px]">
-              <div className="w-full h-[80px]  flex justify-between border-b-2 border-b-2-gray-300 items-center bg-white">
-                  <div className="w-[45%] h-full  flex justify-evenly items-center">
-                        {
-                            menu.map((items)=>(
-                                <>
-                                <div className='w-[100px] h-[42px] hover:border-b-2 border-b-gray-500 mt-[40px] text-center cursor-pointer'>
-                                    {items}{items == "All Posts"?"(32)":""}
-                                </div>
-                                </>
-                            ))
-                            
-                        }
-                  </div>
-                  <div className="w-[35%] h-full  flex justify-center items-center gap-4  ">
-                         <button className="w-[160px] h-[40px] bg-gray-300 ">Write a Post<IoMdArrowDropdown style={{display:"inline"}}/></button>
-                         <button className="w-[160px] h-[40px] bg-blue-600 flex justify-center items-center gap-2 "><BsFillPeopleFill />Join  Group</button>
-                 </div>
-              </div>
-              <div className="w-full min-h-[100vh] mt-3 flex">
-              <div className="w-[70%] min-h-[100vh] flex flex-col gap-2">
-                  {
-                    posts.map((items,index)=>(
-                        <>
-                        <Card key={index}  {...items}/>
-                        </>
-                    ))
-                  }
-              </div>
-              <div className="w-[30%] h-[50px]  flex justify-center items-center">
-                          <div className="w-[250px] h-[40px] bg-white text-black flex border-b-2 border-b-gray-200   justify-center items-center gap-2  ">
-                          <MdOutlineLocationOn/><input type="text" className="w-[200px] h-[30px] outline-none bg-transparent" placeholder='Enter your location'/>
-                          <FaPen/>
-                          </div>
-            </div>
-            </div>
+	const menu = ["All Posts", "Article", "Event", "Education", "Job"];
+	const posts = [
+		{
+			title: "Article",
+			content:
+				"What if famous brands had regular fonts? Meet RegulaRands!",
+			truncatedContent:
+				"I've worked in UX for the better part of a decade. From now on, I plan to rel...",
+			author: "Sarthak Kamra",
+			views: "1.4k",
+			imageUrl: "img1.png",
+			avatarUrl: "p1.png",
+		},
+		{
+			title: "",
+			content:
+				"Tax Benefits for Investment under National Pension Scheme launched for Government",
+			truncatedContent:
+				"I've worked in UX for the better part of a decade. From now on, I plan to rel...",
+			author: "Sushil Misil",
+			views: "1.5k",
+			imageUrl: "img2.png",
+			avatarUrl: "p2.png",
+		},
+		{
+			title: "Twitter",
+			content: "Finance & Investment Elite: Social MNer-@LinkedEd",
+			truncatedContent:
+				"#FinTwits #Fin #Trading #Stocks #Bonds #Investment #BusinessAnalytics #DataScience",
+			author: "Wade Jones",
+			views: "1.7k",
+			imageUrl: "img3.png",
+			avatarUrl: "p3.png",
+		},
+		{
+			title: "",
+			content: "Software Developer",
+			truncatedContent: "@MicrosoftSupport/Helpdesk • 4 hrs",
+			author: "Joseph Gray",
+			views: "5.5k",
+			imageUrl: "",
+			avatarUrl: "p4.png",
+		},
+	];
+	return (
+		<>
+			<div className="md:w-[80%] w-full min-h-[100vh] md:mx-[200px]">
+        <div className="flex md:hidden p-4 items-center justify-between">
+          <span className="text-black font-bold text-xl">
+            Posts (32)
+          </span>
+          <select name="filter" id="filter-select" className="bg-gray-200 rounded-md py-2.5 px-4 font-medium">
+            <option value="All Posts">All Posts</option>
+            <option value="Article">Article</option>
+            <option value="Event">Event</option>
+            <option value="Education">Education</option>
+            <option value="Job">Job</option>
+          </select>
         </div>
-        </>
-    )
+				<div className="w-full h-[80px] md:flex hidden md:flex-row flex-col justify-between border-b-2 border-b-2-gray-300 items-center bg-white">
+					<div className="md:w-[45%] h-full flex justify-evenly items-center">
+						{menu.map((items) => (
+							<>
+								<div className="w-[100px] h-[42px] hover:border-b-2 border-b-gray-500 mt-[40px] text-center cursor-pointer">
+									{items}
+									{items == "All Posts" ? "(32)" : ""}
+								</div>
+							</>
+						))}
+					</div>
+					<div className="md:w-[35%] h-full  md:flex hidden justify-center items-center gap-4  ">
+						<button className="w-[160px] h-[40px] bg-gray-300 ">
+							Write a Post
+							<IoMdArrowDropdown style={{ display: "inline" }} />
+						</button>
+						<button className="w-[160px] h-[40px] bg-blue-600 flex justify-center items-center gap-2 ">
+							<BsFillPeopleFill />
+							Join Group
+						</button>
+					</div>
+				</div>
+				<div className="w-full min-h-[100vh] mt-3 flex">
+					<div className="md:w-[70%] min-h-[100vh] flex flex-col gap-2">
+						{posts.map((items, index) => (
+							<>
+								<Card key={index} {...items} />
+							</>
+						))}
+					</div>
+					<div className="md:w-[30%] h-[50px] md:flex hidden justify-center items-center">
+						<div className="w-[250px] h-[40px] bg-white text-black flex border-b-2 border-b-gray-200   justify-center items-center gap-2  ">
+							<MdOutlineLocationOn />
+							<input
+								type="text"
+								className="w-[200px] h-[30px] outline-none bg-transparent"
+								placeholder="Enter your location"
+							/>
+							<FaPen />
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default Section
+export default Section;
